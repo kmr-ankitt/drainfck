3d8ad887fb62f61f6cbf2274d893b5650ccb58e0
ankit <sahuankit.murhu@gmail.com>
2026-02-23 19:22:31 +0530

    fix: repl multi line input fix

--- source/repl.d
+++ source/repl.d
@@ -2,19 +2,37 @@ module repl;
 
 import std.stdio;
 import std.string;
+import parser;
 
 void run()
 {
   writeln("Welcome to the Drainfck REPL!");
   writeln("Type 'exit' to quit.");
 
+  enum SIZE = 30_000;
+
   while (true)
   {
     write(">>> ");
-    string input = stdin.readln().chomp();
-    writeln(input);
+    string code;
 
-    if (input == "exit")
-      break;
+    while (true)
+    {
+        string line = stdin.readln().chomp();
+
+        if (line == "exit")
+            return;
+
+        if (line == "")
+            break;
+
+        code ~= line ~ "\n";
+    }
+
+    int[SIZE] container;
+    size_t dataCounter = 0;
+    size_t programCounter = 0;
+
+    parser.parse(code, programCounter, dataCounter, container);
   }
 }
