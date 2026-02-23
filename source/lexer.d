63a88bf9259d4f42045264eb69d9e57061ec85f7
ankit <sahuankit.murhu@gmail.com>
2026-02-23 19:08:35 +0530

    feat: lexical analysis of code

--- source/lexer.d
+++ source/lexer.d
@@ -1,6 +1,8 @@
 import lexer;
+import parser;
 
 import std.stdio;
+
 
 void lex(string fileContent)
 {
@@ -9,4 +11,17 @@ void lex(string fileContent)
     writeln("Error: Empty file content.");
     return;
   }
+
+  enum SIZE = 30_000;
+  int[SIZE] container;
+
+  /*
+  * Program counter
+  * - keeps track of where you are in the source code being interpreted
+  * Data counter
+  * - keeps track of where you are in the memory array (container) that holds the
+  */
+  size_t programCounter = 0;
+  size_t dataCounter = 0;
+  parser.parse(fileContent, programCounter, dataCounter, container);
 }
